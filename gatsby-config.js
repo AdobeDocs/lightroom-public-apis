1a176ab9a2d0ff322ba6c915aa935800ceddf9d2
bagra98 <89960477+bagra98@users.noreply.github.com>
2021-09-15 10:17:10 +0530

    Remove support links (#6)

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,36 +104,6 @@ module.exports = {
         path: '/code-sample/'
       },
       {
-        title: 'Overview',
-        path: '/support/',
-        header: true,
-        pages: [
-          {
-            title: 'Help',
-            path: '/support/'
-          },
-          {
-            title: 'FAQ',
-            path: '/support/FAQ/'
-          },
-          {
-            title: 'How to contribute',
-            path: '/support/contribute/'
-          }
-        ]
-      },
-      {
-        title: 'Community',
-        path: '/support/community/',
-        header: true,
-        pages: [
-          {
-            title: 'Information',
-            path: '/support/community/'
-          }
-        ]
-      },
-      {
         title: 'API Change Logs',
         path: '/release-notes/'
       }
